efab719a6166c4e181b316c0574f07bf2473f66e
shweta.swami <shweta.swami@dev.figmd.com>
2019-10-01 18:11:45 +0530

    added rangewidget file

--- src/components/widgets/RangeWidget.js
+++ src/components/widgets/RangeWidget.js
@@ -1,7 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/styles";
 
 import { rangeSpec } from "../../utils";
+//const classes = PropTypes.object.isRequired;
+
+const styles = theme => ({
+  min_label: {
+    textAlign: "right",
+    clear: "both",
+    float: "left",
+  },
+  baseInput_margin: {
+    marginLeft: "13px",
+    width: "95% !important",
+  },
+  max_label: {
+    float: "right",
+    marginTop: "-22px",
+  },
+});
 
 function RangeWidget(props) {
   const {
@@ -11,15 +29,19 @@ function RangeWidget(props) {
       widgets: { BaseInput },
     },
   } = props;
+  const { classes } = props;
 
   return (
     <div className="field-range-wrapper">
+      <label className={classes.min_label}>{props.schema.minimum}</label>
       <BaseInput
         type="range"
+        className={classes.baseInput_margin}
         {...props}
         value={value ? value : 0}
         {...rangeSpec(schema)}
       />
+      <label className={classes.max_label}>{props.schema.maximum}</label>
       <span className="range-view">{value}</span>
     </div>
   );
@@ -28,7 +50,8 @@ function RangeWidget(props) {
 if (process.env.NODE_ENV !== "production") {
   RangeWidget.propTypes = {
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    classes: PropTypes.object.isRequired,
   };
 }
 
-export default RangeWidget;
+export default withStyles(styles)(RangeWidget);
